b11821c15258f0bdfdf45b638e3048388658f490
Sabrina Ellen <ellen.sabrina100@hotmail.com>
2022-06-27 11:19:14 -0300

    Game Mania Responsivo com Login

---
+++ javascript/index.js
@@ -0,0 +1,34 @@
+//menu mobile
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+//slide carrosel
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function () {
+    nextImage();
+}, 5000)
+
+function nextImage() {
+    count++;
+    if (count > 6) {
+        count = 1;
+    }
+    document.getElementById("radio" + count).checked = true;
+}
